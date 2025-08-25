2d42f6133bb0eea875a96e2b24a9ba6c3937fa3d
PedroH-Neves <pedrohdn710@gmail.com>
2025-08-25 09:19:57 -0300

    feat: add quiz functionality

--- main.go
+++ main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	//ler o arquivo csv
-	//printar as perguntas na tela
-	//receber a resposta do usuario
-	//verificar se a resposta do usuario e igual a do arquivo
-	//salvar se esta certo ou errada
-	//printar quantidade correta e incorreta
+	fileName := flag.String("filename", "problems.csv", "csv de duas colunas perguntas e respostas")
+	flag.Parse()
 
-	fmt.Println("quiz game start")
+	fmt.Println("Welcome to the quiz game!!!")
 
-	file, err := os.Open("problems.csv")
+	file, err := os.Open(*fileName)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close() //defer roda no final da execucao da funcao
+	defer file.Close()
 
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
@@ -29,4 +26,22 @@ func main() {
 	}
 
 	fmt.Println(records)
+
+	totalQuestions := len(records)
+	correctAnswers := 0
+
+	for _, record := range records {
+		question := record[0]
+		answer := record[1]
+		var userAnswer string
+		fmt.Println("Question:", question)
+		fmt.Scan(&userAnswer)
+		correctAnswer := userAnswer == answer
+		if correctAnswer {
+			correctAnswers += 1
+		}
+		fmt.Println("Your answer is:", userAnswer)
+	}
+	result := totalQuestions - correctAnswers
+	fmt.Printf("This is your score:\n Right answers:%d\n Wrong answers:%d\n", correctAnswers, result)
 }
