3153e7a2a6ac21183c94b7404114fb8e05e7159c
PedroH-Neves <pedrohdn710@gmail.com>
2025-09-02 15:52:21 -0300

    feat:add 30 sec timer

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	fileName := flag.String("filename", "problems.csv", "csv de duas colunas perguntas e respostas")
 	flag.Parse()
 
+	timer30 := time.NewTimer(30 * time.Second)
+
 	fmt.Println("Welcome to the quiz game!!!")
+	fmt.Println("You will have 30 seconds to answer the questions")
+	fmt.Println("The time is running")
 
 	file, err := os.Open(*fileName)
 	if err != nil {
@@ -28,18 +33,32 @@ func main() {
 	totalQuestions := len(records)
 	correctAnswers := 0
 
+questionLoop:
 	for _, record := range records {
 		question := record[0]
 		answer := record[1]
-		var userAnswer string
+
 		fmt.Println("Question:", question)
-		fmt.Scan(&userAnswer)
-		correctAnswer := userAnswer == answer
-		if correctAnswer {
-			correctAnswers += 1
+
+		answerCh := make(chan string)
+
+		go func() {
+			var userAnswer string
+			fmt.Scan(&userAnswer)
+			answerCh <- userAnswer
+		}()
+
+		select {
+		case <-timer30.C:
+			fmt.Println("Sorry, the time is up!!")
+			break questionLoop
+		case userAnswer := <-answerCh:
+			if userAnswer == answer {
+				correctAnswers++
+			}
+
 		}
-		fmt.Println("Your answer is:", userAnswer)
 	}
-	result := totalQuestions - correctAnswers
-	fmt.Printf("This is your score:\n Right answers:%d\n Wrong answers:%d\n", correctAnswers, result)
+	wrongAnswers := totalQuestions - correctAnswers
+	fmt.Printf("This is your score:\n Right answers:%d\n Wrong answers:%d\n", correctAnswers, wrongAnswers)
 }
